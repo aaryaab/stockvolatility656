ec138010b1ffc10f766dd80f15bc5ab80ed764a8
K S N S Gopala Krishna <gopalakrishna.kala@gmail.com>
2023-12-01 02:15:44 -0600

    Add files via upload

--- main1.py
+++ main1.py
@@ -11,11 +11,17 @@ import pylab as pl
 from pandas.plotting import lag_plot
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.seasonal import seasonal_decompose
-###########
-# sidebar #
-###########
+
+
 st.set_page_config(layout="wide",)
-option = st.sidebar.selectbox('Select one symbol', ( 'LUV', 'NVDA',"AMZN",'WMT'))
+
+st.sidebar.markdown("Select the Stock you want to Analyze below:")
+st.sidebar.markdown("LUV is for Southwest Airlines")
+st.sidebar.markdown("NVDA is for NVIDIA")
+st.sidebar.markdown("AMZN is for Amazon")
+st.sidebar.markdown("WMT is for Walmart")
+
+option = st.sidebar.selectbox('Select one stock', ( 'LUV', 'NVDA',"AMZN",'WMT'))
 import datetime
 today = datetime.date.today()
 before = today - datetime.timedelta(days=700)
@@ -26,20 +32,17 @@ if start_date < end_date:
 else:
     st.sidebar.error('Error: End date must fall after start date.')
 
-nolags = st.sidebar.selectbox('Select the lags you want', ( 5,1,2,3,4,6,7,8,9,10))
+number = st.sidebar.number_input('Enter the number of days of recent data you want', min_value=1, value=10)
+window = st.sidebar.number_input('Select the rolling window for volatality of Stock', min_value=2, value=12)
+#window = st.sidebar.selectbox('', (30,10,12,20,40,50,60,70,80,90,100))
+nolags = st.sidebar.selectbox('Select the lags you want', (5,1,2,3,4,6,7,8,9,10))
 
-##############
-# Stock data #
-##############
 
-# Download data
+
 df = yf.download(option,start= start_date,end= end_date, progress=False)
 
 df_stock=df.drop(['Open','High','Low','Adj Close','Volume'],axis=1)
 
-###################
-# Set up main app #
-###################
 if option == 'LUV':
     st.markdown("<h1 style='text-align: center; color: black;'>Southwest Airlines Stock Closing Prices over time</h1>", unsafe_allow_html=True)
 elif option == 'NVDA':
@@ -48,35 +51,67 @@ elif option == 'AMZN':
     st.markdown("<h1 style='text-align: center; color: black;'>Amazon Stock Closing Prices over time</h1>", unsafe_allow_html=True)
 elif option == 'WMT':
     st.markdown("<h1 style='text-align: center; color: black;'>Walmart Stock Closing Prices over time</h1>", unsafe_allow_html=True)
-#st.write('Stock Closing Prices over time')
 st.line_chart(df_stock)
 
 progress_bar = st.progress(0)
 
-# Plot MACD
-##st.write('Stock Moving Average Convergence Divergence (MACD)')
-##st.area_chart(macd)
 
-# Plot RSI
-##st.write('Stock RSI ')
-##st.line_chart(rsi)
 
-# Data of recent days
+
 st.markdown("<h1 style='text-align: center; color: black;'>Recent Data of the selected Stock</h1>", unsafe_allow_html=True)
-st.dataframe(df.tail(10))
+
+def recent_data(number):
+    if isinstance(number, int) and number > 0:
+        st.dataframe(df.tail(number))
+    else:
+        st.error("Please enter a valid positive integer.")
+
+recent_data(number)
 
 
+if option == 'LUV':
+    st.markdown("<h1 style='text-align: center; color: black;'>Southwest Airlines Daily Returns</h1>", unsafe_allow_html=True)
+elif option == 'NVDA':
+    st.markdown("<h1 style='text-align: center; color: black;'>NVDIA Daily Returns</h1>", unsafe_allow_html=True)
+elif option == 'AMZN':
+    st.markdown("<h1 style='text-align: center; color: black;'>Amazon Daily Returns</h1>", unsafe_allow_html=True)
+elif option == 'WMT':
+    st.markdown("<h1 style='text-align: center; color: black;'>Walmart Daily Returns</h1>", unsafe_allow_html=True)
+##st.line_chart(df_stock)
+
+
+df['daily_returns']=(df['Close'].pct_change())*100
+fig_daily,ax=plt.subplots(figsize=(12,6))
+ax.spines[['top','right','left','bottom']].set_visible(False)
+plt.plot(df['daily_returns'], label = 'Daily Returns')
+plt.legend(loc='best')
+st.pyplot(fig_daily)
 
 avg_stock = df_stock['Close'].mean()
 if option == 'LUV':
-    st.markdown(f"<h3 style='text-align: left; color: black;'>Average Stock Price of Southwest Airlines is {avg_stock:.2f}</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h3 style='text-align: left; color: black;'>Average Stock Price of Southwest Airlines is {avg_stock:.2f}</h3>", unsafe_allow_html=True)
 elif option == 'NVDA':
-    st.markdown(f"<h3 style='text-align: left; color: black;'>Average Stock Price of NVDIA Airlines is {avg_stock:.2f}</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h3 style='text-align: left; color: black;'>Average Stock Price of NVDIA Airlines is {avg_stock:.2f}</h3>", unsafe_allow_html=True)
 elif option == 'AMZN':
-    st.markdown(f"<h3 style='text-align: left; color: black;'>Average Stock Price of Amazon is {avg_stock:.2f}</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h3 style='text-align: left; color: black;'>Average Stock Price of Amazon is {avg_stock:.2f}</h3>", unsafe_allow_html=True)
 elif option == 'WMT':
-    st.markdown(f"<h3 style='text-align: left; color: black;'>Average Stock Price of Walmart is {avg_stock:.2f}</h1>", unsafe_allow_html=True)
+    st.markdown(f"<h3 style='text-align: left; color: black;'>Average Stock Price of Walmart is {avg_stock:.2f}</h3>", unsafe_allow_html=True)
 
+
+def plot_stock_volatility(stock_df, window):
+    daily_returns = stock_df['Close'].pct_change()
+    
+    rolling_std = daily_returns.rolling(window=window).std()
+    
+    plt.figure(figsize=(12, 6))
+    rolling_std.plot()
+    plt.title(f'Volatility of {option} Stock (Rolling {window}-Day Std Dev)')
+    plt.xlabel('Date')
+    plt.ylabel('Volatility (Std Dev)')
+    plt.grid(True)
+    st.pyplot(plt)
+
+plot_stock_volatility(df_stock, window)
 
 def autocorr(timeseries, nolags):
     fig1 = plt.figure()
@@ -91,10 +126,8 @@ autocorr(df_stock, nolags)
 
 #Test for staionarity
 def test_stationarity(timeseries):
-    #Determing rolling statistics
     rolmean = timeseries.rolling(12).mean()
     rolstd = timeseries.rolling(12).std()
-    #Plot rolling statistics:
     fig = plt.figure()
     plt.plot(timeseries, color='blue',label='Original')
     plt.plot(rolmean, color='red', label='Rolling Mean')
@@ -109,32 +142,12 @@ def test_stationarity(timeseries):
     for key,values in adft[4].items():
         output['critical value (%s)'%key] =  values
     st.pyplot(fig)
-    center_css = """
-    <style>
-        .dataframe th, .dataframe td {
-            text-align: center !important;
-        }
-    </style>
-    """
 
-    # Inject CSS with Markdown
-    st.markdown(center_css, unsafe_allow_html=True)
-
-    # Display DataFrame
+    
+   
     st.markdown("<h1 style='text-align: center; color: black;'>Statistic values of Dickey Filler Test</h1>", unsafe_allow_html=True)
     st.dataframe(output)
-
-    #st.markdown("<h1 style='text-align: center; color: black;'>Statistic values of Dickey Filler Test</h1>", unsafe_allow_html=True)
-    #st.write('Statistic values of Dickey Filler Test')
-    
-    #st.dataframe(output)
-
-    # ... [rest of your code]
-
-
-    ##st.dataframe(output)
     return output
 
-##st.write('Results of Dickey Filler Test')
 test_stationarity(df_stock)
 
